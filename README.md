06c06a3dc11f3f6e1705224ba74f5e4f8dd93558
DZvancuks <dmitrijs.zvancuks@gmail.com>
2019-07-21 12:58:30 +0300

    Update description

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # CMPLS
-CMPLS - Complete the Sequence. Pure C project to solve https://www.spoj.com/problems/CMPLS/ problem.
+CMPLS - Complete the Sequence. 
+Pure C project to solve https://www.spoj.com/problems/CMPLS/ problem.
+
+https://en.wikipedia.org/wiki/Difference_engine#Method_of_differences
+Sequence of length S must be extrapolated C steps
+1: Derive sequence until D != 0
+P(n)    -> 1 2 4 7 11 16 22 29 -> D > 0
+P'(n)   -> 1 2 3 4  5  6  7    -> D > 0
+P''(n)  -> 1 1 1 1  1  1       -> D = 0 ()
+P'''(n) -> 0 0 0 0  0          -> D = -1 (can be ignored)
+
+2: Take values last in orders
+29 7 1
+
+3: Append all values from back to front. Repeat C times
+37 8 1
+46 9 1
+
--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
-// https://en.wikipedia.org/wiki/Difference_engine#Method_of_differences
-#define MAX_ORDER 10
+/* https://en.wikipedia.org/wiki/Difference_engine#Method_of_differences
+ * Sequence of length S must be extrapolated C steps
+ * 1: Derive sequence until D != 0
+ * P(n)    -> 1 2 4 7 11 16 22 29 -> D > 0
+ * P'(n)   -> 1 2 3 4  5  6  7    -> D > 0
+ * P''(n)  -> 1 1 1 1  1  1       -> D = 0 ()
+ * P'''(n) -> 0 0 0 0  0          -> D = -1 (can be ignored)
+ *
+ * 2: Take values last in orders
+ * 29 7 1
+ *
+ * 3: Append all values from back to front. Repeat C times
+ * 37 8 1
+ * 46 9 1
+ */
+
+#define MAX_ORDER 100
 #define MAX_SEQ 100
 
 void derive_order( int * sequence, int len )
